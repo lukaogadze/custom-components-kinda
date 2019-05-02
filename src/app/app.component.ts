62e648ede9cba3eaa73992882ee708e2edbfd84e
holo <holo@HOLO-PC>
2019-05-02 08:03:17 +0400

    INITIAL

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    date!: FormControl;
+
+    dump() {
+        console.log(this.date.value.format("DD/MM/YYYY"));
+    }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {
+    DateAdapter,
+    MAT_DATE_LOCALE,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MAT_DATE_FORMATS} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+
+export const KA_FORMAT = {
+    parse: {
+        dateInput: 'DD/MM/YYYY',
+    },
+    display: {
+        dateInput: 'DD/MM/YYYY',
+        monthYearLabel: 'MMM YYYY',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'MMMM YYYY',
+    },
+};
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NoopAnimationsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatButtonModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+        {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+        {provide: MAT_DATE_FORMATS, useValue: KA_FORMAT}
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
